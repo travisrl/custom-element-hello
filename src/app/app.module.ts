5537f0da8206ec1bf8c20e7fdf257394d4d116ea
Travis Larson <admin@travislarson.com>
2019-04-12 12:19:28 -0400

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Injector } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { MatButtonModule, MatCheckboxModule, MatInputModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {createCustomElement} from '@angular/elements';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatInputModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatCheckboxModule,
+    MatInputModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [AppComponent]
 })
-export class AppModule { }
+
+export class AppModule {
+
+	constructor(private injector: Injector) {}
+
+  	ngDoBootstrap() {
+    	const el = createCustomElement(AppComponent, 
+    		{ injector: this.injector });
+    	customElements.define('hello-world', el);
+	}
+}
